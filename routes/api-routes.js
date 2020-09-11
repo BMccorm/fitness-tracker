1a7322b3c13d7469a4c0797173448305230b2b58
Brielle McCormick <briellemccormick@Brielles-MBP.fios-router.home>
2020-09-10 23:02:17 -0400

    fixed api routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -2,20 +2,26 @@ const router = require("express").Router();
 const Workout = require("../models/workout.js");
 
 router.put("/api/workouts/:id", (req, res) => {
-    Workout.findOneAndUpdate({ _id: req.params.id }, req.body, function (err, dbWorkout) {
-        res.send(dbWorkout);
-    });
+    Workout.findOneAndUpdate({ _id: req.params.id }, { $push: { exercises: req.body } })
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
 });
 
-// router.post("/api/transaction/bulk", ({ body }, res) => {
-//     Workout.insertMany(body)
-//         .then(dbTransaction => {
-//             res.json(dbTransaction);
-//         })
-//         .catch(err => {
-//             res.status(400).json(err);
-//         });
-// });
+
+router.post("/api/workouts", (req, res) => {
+    Workout.create({})
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+
+})
 
 router.get("/api/workouts/range", (req, res) => {
     Workout.find({})
